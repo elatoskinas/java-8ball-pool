7b65d61658417c4b24b468e45170f4d182244b35
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2019-11-12 16:40:51 +0100

    Setup LibGDX with core & desktop packages

---
+++ desktop/src/com/sem/pool/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sem.pool.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sem.pool.Pool;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pool(), config);
+	}
+}
