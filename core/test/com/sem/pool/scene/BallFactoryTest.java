edd74db336426b42f2c1002880d5cd57bffb62d3
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2019-11-19 18:41:08 +0100

    Add test for ball creation in BallFactory class

--- core/test/com/sem/pool/scene/BallFactoryTest.java
+++ core/test/com/sem/pool/scene/BallFactoryTest.java
@@ -1,11 +1,13 @@
 package com.sem.pool.scene;
 
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +17,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class BallFactoryTest {
     BallFactory factory;
     List<Texture> textures;
+    AssetManager assetManager;
 
     @BeforeEach
     public void setup() {
         textures = new ArrayList<Texture>();
-        factory = new BallFactory(textures);
+        assetManager = Mockito.mock(AssetManager.class);
+        factory = new BallFactory(textures, assetManager);
+    }
+
+    @Test
+    public void testCreateBall() {
+        final int id = 0;
+        final Model model = Mockito.mock(Model.class);
+
+        Ball3D expectedBall = new Ball3D(id, model);
+        Ball3D ball = factory.createBall(id);
+
+        assertEquals(expectedBall, ball);
     }
 }
