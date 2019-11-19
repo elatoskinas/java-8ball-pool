6743490b18e429a0bba8f3af6dd491dc928c4859
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2019-11-19 18:10:10 +0100

    Implement constructor for Ball3D class

--- core/src/com/sem/pool/Ball3D.java
+++ core/src/com/sem/pool/Ball3D.java
@@ -5,4 +5,17 @@ import com.badlogic.gdx.graphics.g3d.Model;
 public class Ball3D {
     private int id;
     private Model model;
+
+    public Ball3D(int id, Model model) {
+        this.id = id;
+        this.model = model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Model getModel() {
+        return model;
+    }
 }
