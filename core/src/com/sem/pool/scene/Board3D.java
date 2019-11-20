236833f168986db48b50dad17fc7bd76c901d6b5
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2019-11-20 21:33:36 +0100

    Documentation for Board3D implementation

--- core/src/com/sem/pool/scene/Board3D.java
+++ core/src/com/sem/pool/scene/Board3D.java
@@ -2,9 +2,16 @@ package com.sem.pool.scene;
 
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 
+/**
+ * Class representing a 3D Board of a single game.
+ */
 public class Board3D {
     private transient ModelInstance model;
 
+    /**
+     * Constructs a new 3D Board instance with the specified model.
+     * @param model  Model object of the Board
+     */
     public Board3D(ModelInstance model) {
         this.model = model;
     }
