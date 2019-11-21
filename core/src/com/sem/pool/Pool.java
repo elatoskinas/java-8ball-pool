abcab8b3e9a721287fb15b6e57483393e2c759d9
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2019-11-21 16:34:30 +0100

    Cleanup main Pool class

--- core/src/com/sem/pool/Pool.java
+++ core/src/com/sem/pool/Pool.java
@@ -11,45 +11,19 @@ import com.sem.pool.scene.AssetLoader;
 import com.sem.pool.scene.Scene3D;
 
 public class Pool extends ApplicationAdapter {
-    //SpriteBatch batch;
-    //Texture img;
-    Scene3D scene;
-
     @Override
     public void create() {
-        //batch = new SpriteBatch();
-        //img = new Texture("badlogic.jpg");
-        AssetManager manager = new AssetManager();
-        AssetLoader loader = new AssetLoader(manager);
-        loader.initializeModels();
-        ModelBatch modelBatch = new ModelBatch();
 
-        scene = new Scene3D(loader, modelBatch);
     }
 
     @Override
     public void render() {
-        if (scene.getAssetLoader().getAssetManager().update()) {
-            scene.instantiate();
-            scene.getCamera().position.set(0f, 5f, 0f);
-            scene.getCamera().lookAt(0,0,0);
-            scene.getCamera().near = 1f;
-            scene.getCamera().far = 300f;
-            scene.getCamera().update();
-        }
-
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-        //batch.begin();
-        //batch.draw(img, 0, 0);
-        //batch.end();
-        if (scene.getModels() != null) scene.render();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
     public void dispose() {
-        //batch.dispose();
-        //img.dispose();
+
     }
 }
