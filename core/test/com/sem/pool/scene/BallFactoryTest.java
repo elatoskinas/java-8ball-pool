3807c23e525d5419f493004bbeb6f752a43b2a32
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2019-11-24 19:05:21 +0100

    Add test cases for BallFactory Ball texture assignment

--- core/test/com/sem/pool/scene/BallFactoryTest.java
+++ core/test/com/sem/pool/scene/BallFactoryTest.java
@@ -3,9 +3,12 @@ package com.sem.pool.scene;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -69,5 +72,53 @@ class BallFactoryTest {
         factory.setTextures(newTextures);
 
         assertEquals(newTextures, factory.getTextures());
+    }
+
+    @Test
+    public void testBallTextureAssignedCueBall() {
+        final int ballId = 0;
+        final int textureId = 0;
+        final int textureCount = 2;
+
+        testBallTextureAssignHelper(ballId, textureId, textureCount);
+    }
+
+    @Test
+    public void testBallTextureAssignedNonCueBall() {
+        final int ballId = 2;
+        final int textureId = 2;
+        final int textureCount = 4;
+
+        testBallTextureAssignHelper(ballId, textureId, textureCount);
+    }
+
+    @Test
+    public void testBallTextureAssignedLastBall() {
+        final int ballId = 3;
+        final int textureId = 3;
+        final int textureCount = 4;
+
+        testBallTextureAssignHelper(ballId, textureId, textureCount);
+    }
+
+    private void testBallTextureAssignHelper(int ballId, int textureId, int textureCount) {
+        for (int i = 0; i < textureCount; ++i) {
+            Texture mockTexture = Mockito.mock(Texture.class);
+            textures.add(mockTexture);
+        }
+
+        final ModelInstance model = Mockito.mock(ModelInstance.class);
+        final Material material = Mockito.mock(Material.class);
+        final String materialId = "0";
+
+        Mockito.when(assetLoader.loadModel(BallFactory.MODEL_TYPE)).thenReturn(model);
+        Mockito.when(model.getMaterial(materialId)).thenReturn(material);
+
+        Texture texture = textures.get(textureId);
+        TextureAttribute attribute = TextureAttribute.createDiffuse(texture);
+
+        Ball3D ball = factory.createBall(ballId);
+
+        Mockito.verify(material).set(attribute);
     }
 }
