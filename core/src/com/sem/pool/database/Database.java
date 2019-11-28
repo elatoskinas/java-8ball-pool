fe05d148ce9ecc51d1fb13281a4132e2283540bb
Dany Sluijk <D.Sluijk@student.tudelft.nl>
2019-11-28 21:24:03 +0100

    docs: add class documentation

--- core/src/com/sem/pool/database/Database.java
+++ core/src/com/sem/pool/database/Database.java
@@ -7,6 +7,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
 
+/**
+ * Handle the database.
+ */
 public class Database {
     /**
      * The database object singleton.
