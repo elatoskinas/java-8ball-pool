98057dd2c00c109ce1bc41f96e8f156e950180d0
Thomas Crul <T.C.Crul@student.tudelft.nl>
2019-11-28 16:31:02 +0100

    constructor tests

--- core/test/com/sem/pool/GameTest.java
+++ core/test/com/sem/pool/GameTest.java
@@ -1,4 +1,27 @@
 package com.sem.pool;
 
+import com.badlogic.gdx.Input;
+import com.sem.pool.scene.Scene3D;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameTest {
+
+    /**
+     * Test method to verify that the Game object is instantiated correctly.
+     * (Scene and input are assigned without error)
+     */
+    @Test
+    void testConstructor() {
+        Scene3D scene = Mockito.mock(Scene3D.class);
+        Input input = Mockito.mock(Input.class);
+        
+        Game game = new Game(scene, input);
+        
+        assertEquals(scene, game.getScene());
+        assertEquals(input, game.getInput());
+    }
+    
 }
