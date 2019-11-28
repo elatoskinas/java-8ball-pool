e933ce8f8259c6e8a155c9690aba80224b8d36ee
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2019-11-28 15:59:20 +0100

    Add missing JavaDoc comments in DesktopLauncher class

--- desktop/src/com/sem/pool/desktop/DesktopLauncher.java
+++ desktop/src/com/sem/pool/desktop/DesktopLauncher.java
@@ -4,7 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.sem.pool.Pool;
 
+/**
+ * Launcher to launch the Game application on a Desktop platform.
+ */
 public class DesktopLauncher {
+    /**
+     * Starts the Game application on the DesktopLauncher.
+     * @param arg  Optional arguments (unused in the Game application)
+     */
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         new LwjglApplication(new Pool(), config);
