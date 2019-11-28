fcb253fe75307a5bc7c5a9424c38f723d4c54a31
Thomas Crul <T.C.Crul@student.tudelft.nl>
2019-11-28 16:34:08 +0100

    constructor and getter implementation

--- core/src/com/sem/pool/Game.java
+++ core/src/com/sem/pool/Game.java
@@ -1,4 +1,22 @@
 package com.sem.pool;
 
+import com.badlogic.gdx.Input;
+import com.sem.pool.scene.Scene3D;
+
 public class Game {
+    Scene3D scene;
+    Input input;
+    
+    public Game(Scene3D scene, Input input) {
+        this.scene = scene;
+        this.input = input;
+    }
+
+    public Scene3D getScene() {
+        return scene;
+    }
+
+    public Input getInput() {
+        return input;
+    }
 }
