ac78b954c76588f44d9b3b310cab8bae2573ceb2
Pepijn Klop <p.e.f.klop@student.tudelft.nl>
2019-11-29 10:00:21 +0100

    Add TODO's and javadoc

--- core/src/com/sem/pool/scene/Cue3D.java
+++ core/src/com/sem/pool/scene/Cue3D.java
@@ -5,13 +5,15 @@ import com.badlogic.gdx.math.Vector3;
 
 /**
  * Class representing a 3D Cue.
+ * TODO: Rotation of the cue.
+ * TODO: Set position according to the force.
  */
 public class Cue3D {
 
     /**
      * The distance between the center of the cueBall and the tip of the cue.
      */
-    private static final float cueRadius = 0.3f;
+    private static final float cueRadius = 0.2f;
 
     private transient ModelInstance model;
 
@@ -23,8 +25,13 @@ public class Cue3D {
         return model;
     }
 
+
+    /**
+     * Set the cue left from the cue ball.
+     * @param cueBall Ball3D cue ball to determine position.
+     */
     public void toShotPosition(Ball3D cueBall) {
         Vector3 position = cueBall.getModel().transform.getTranslation(new Vector3());
-        model.transform.translate(position.x - cueRadius, position.y, position.z);
+        model.transform.translate(position.x - cueRadius, position.y + 0.1f, position.z);
     }
 }
