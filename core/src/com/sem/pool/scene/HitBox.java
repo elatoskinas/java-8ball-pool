3c696da74d28d925ecae594329054328de4d792c
Thomas Crul <T.C.Crul@student.tudelft.nl>
2019-12-03 16:44:38 +0100

    constructor + getters HitBox

--- core/src/com/sem/pool/scene/HitBox.java
+++ core/src/com/sem/pool/scene/HitBox.java
@@ -11,5 +11,23 @@ public class HitBox {
     
     private transient btCollisionShape shape;
     private transient btCollisionObject object;
+
+    /**
+     * Constructs a new HitBox object with the specified Shape and Collision Object.
+     * @param shape Collision Shape of the HitBox.
+     * @param object Collision Object of the HitBox.
+     */
+    public HitBox(btCollisionShape shape, btCollisionObject object) {
+        this.shape = shape;
+        this.object = object;
+    }
+
+    public btCollisionShape getShape() {
+        return shape;
+    }
+
+    public btCollisionObject getObject() {
+        return object;
+    }
     
 }
