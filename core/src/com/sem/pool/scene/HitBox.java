fbec48ba0fb2ed0309650d3231b7b87f8afb8ad8
Thomas Crul <T.C.Crul@student.tudelft.nl>
2019-12-03 16:34:03 +0100

    HitBox params

--- core/src/com/sem/pool/scene/HitBox.java
+++ core/src/com/sem/pool/scene/HitBox.java
@@ -1,4 +1,11 @@
 package com.sem.pool.scene;
 
+import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
+
 public class HitBox {
+    
+    private transient btCollisionShape shape;
+    private transient btCollisionObject object;
+    
 }
