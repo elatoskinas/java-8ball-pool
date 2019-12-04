99ccb6fa4ff031b9b9623c62094c5542c3a17b03
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2019-12-04 13:01:28 +0100

    Add boilerplate code in Player class according to UML specification

--- core/src/com/sem/pool/game/Player.java
+++ core/src/com/sem/pool/game/Player.java
@@ -1,4 +1,35 @@
 package com.sem.pool.game;
 
+import com.sem.pool.scene.Ball3D;
+import com.sem.pool.scene.RegularBall3D;
+
+import java.util.Set;
+
 public class Player {
+    private int id;
+    private RegularBall3D.Type ballType;
+    Set<RegularBall3D> pottedBalls;
+
+    /**
+     * Creates a new Player with the specified id.
+     * @param id  ID to assign to the player
+     */
+    public Player(int id) {
+
+    }
+
+    /**
+     * Assigns the specified ball type to the Player.
+     * @param ballType  Ball Type to assign to the Player.
+     */
+    public void assignBallType(RegularBall3D.Type ballType) {
+        // TODO: Implement ball type assignment
+        // TODO: Take into account that the ball type should not
+        //       be assigned during the break shot
+        // TODO: Do not assign ball type when cue ball is potted
+    }
+
+    public void potBall(Ball3D ball) {
+        // TODO: Implement ball potting logic
+    }
 }
