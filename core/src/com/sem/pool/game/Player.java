3d7b94217a6f894338d5117987580b5f59089b0b
Pepijn Klop <p.e.f.klop@student.tudelft.nl>
2019-12-19 12:16:25 +0100

    Add unassigned balltype to initialized players

--- core/src/com/sem/pool/game/Player.java
+++ core/src/com/sem/pool/game/Player.java
@@ -3,19 +3,22 @@ package com.sem.pool.game;
 import com.sem.pool.scene.Ball3D;
 import com.sem.pool.scene.RegularBall3D;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Player {
     private int id;
     private RegularBall3D.Type ballType;
-    Set<RegularBall3D> pottedBalls;
+    public Set<RegularBall3D> pottedBalls;
 
     /**
      * Creates a new Player with the specified id.
      * @param id  ID to assign to the player
      */
     public Player(int id) {
-
+        this.id = id;
+        this.ballType = RegularBall3D.Type.UNASSIGNED;
+        this.pottedBalls = new HashSet<>();
     }
 
     /**
