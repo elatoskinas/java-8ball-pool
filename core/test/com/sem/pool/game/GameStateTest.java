ad024b0a90578f5340412c40b97c7947dec13f22
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2020-01-06 23:18:01 +0100

    Fix checkstyle & add JavaDoc for Game & GameState observer pattern

--- core/test/com/sem/pool/game/GameStateTest.java
+++ core/test/com/sem/pool/game/GameStateTest.java
@@ -3,7 +3,6 @@ package com.sem.pool.game;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.sem.pool.scene.Ball3D;
@@ -136,119 +135,6 @@ class GameStateTest {
 
         assertTrue(gameState.isStarted());
     }
-
-    /**
-     * Test case to verify that adding an observer to the observers of the
-     * GameState adds the observer successfully.
-     */
-//    @Test
-//    void testAddObserver() {
-//        assertTrue(gameState.getObservers().isEmpty());
-//
-//        GameStateObserver observer = Mockito.mock(GameStateObserver.class);
-//        gameState.addObserver(observer);
-//
-//        assertTrue(gameState.getObservers().contains(observer));
-//    }
-
-    /**
-     * Test case to verify that removing an observer from the observers
-     * of the GameState successfully removes it from the observers.
-     */
-//    @Test
-//    void testRemoveObserver() {
-//        GameStateObserver observer = Mockito.mock(GameStateObserver.class);
-//
-//        gameState.addObserver(observer);
-//        assertFalse(gameState.getObservers().isEmpty());
-//
-//        gameState.removeObserver(observer);
-//        assertTrue(gameState.getObservers().isEmpty());
-//    }
-
-    /**
-     * Test case to verify that removing an observer from the GameState
-     * that does not exist does not affect the observers collection
-     * of the Game State.
-     */
-//    @Test
-//    void testRemoveObserverNonExistent() {
-//        GameStateObserver observer = Mockito.mock(GameStateObserver.class);
-//        GameStateObserver observer2 = Mockito.mock(GameStateObserver.class);
-//
-//        gameState.addObserver(observer);
-//        assertFalse(gameState.getObservers().isEmpty());
-//
-//        gameState.removeObserver(observer2);
-//        assertFalse(gameState.getObservers().isEmpty());
-//    }
-
-    /**
-     * Test case to verify that adding multiple observers to the
-     * GameState successfully adds them all.
-     */
-//    @Test
-//    void testAddMultipleObservers() {
-//        final int observers = 5;
-//
-//        for (int i = 0; i < observers; ++i) {
-//            GameStateObserver observer = Mockito.mock(GameStateObserver.class);
-//            gameState.addObserver(observer);
-//        }
-//
-//        assertEquals(observers, gameState.getObservers().size());
-//    }
-
-    /**
-     * Test case to verify that when the Player wins,
-     * the Game is stopped and the single attached
-     * observer is notified of the victory.
-     */
-//    @Test
-//    void testPlayerWinOneObserver() {
-//        final int winnerId = 1;
-//        final Player winner = gameState.getPlayers().get(winnerId);
-//
-//        // Add observer to the GameState
-//        GameStateObserver observer = Mockito.mock(GameStateObserver.class);
-//        gameState.addObserver(observer);
-//
-//        // Make the winnerId-th player win the game
-//        gameState.winGame(winnerId);
-//
-//        // Verify that the observer is notified of the winner
-//        Mockito.verify(observer).endGame(winner);
-//
-//        // Assert that game is stopped
-//        assertFalse(gameState.isStarted());
-//    }
-
-    /**
-     * Test case to verify that when the Player wins,
-     * the Game is stopped and the two attached
-     * observers are notified of the victory.
-     */
-//    @Test
-//    void testPlayerWinTwoObservers() {
-//        final int winnerId = 0;
-//        final Player winner = gameState.getPlayers().get(winnerId);
-//
-//        // Add observers to the GameState
-//        GameStateObserver observer = Mockito.mock(GameStateObserver.class);
-//        GameStateObserver observer2 = Mockito.mock(GameStateObserver.class);
-//        gameState.addObserver(observer);
-//        gameState.addObserver(observer2);
-//
-//        // Make the winnerId-th player win the game
-//        gameState.winGame(winnerId);
-//
-//        // Verify that the observers are notified of the winner
-//        Mockito.verify(observer).endGame(winner);
-//        Mockito.verify(observer2).endGame(winner);
-//
-//        // Assert that game is stopped
-//        assertFalse(gameState.isStarted());
-//    }
 
     /**
      * Test case to verify that potting a ball via the
