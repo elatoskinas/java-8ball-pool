94e004774bab4a222c1d99e56b4c961945f56e8b
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2020-01-08 16:36:05 +0100

    Import assert not null in GameState test class

--- core/test/com/sem/pool/game/GameStateTest.java
+++ core/test/com/sem/pool/game/GameStateTest.java
@@ -3,6 +3,7 @@ package com.sem.pool.game;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.sem.pool.scene.Ball3D;
