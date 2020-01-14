d98f8a54075b7b28cd7fda848db767e2bd4d63dd
Evaldas Latoskinas <E.Latoskinas@student.tudelft.nl>
2020-01-14 14:50:42 +0100

    Refactor Pool class to make use of GameInitializer for testing

--- core/src/com/sem/pool/screens/Pool.java
+++ core/src/com/sem/pool/screens/Pool.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.Bullet;
 import com.sem.pool.factories.AssetLoader;
@@ -13,6 +14,7 @@ import com.sem.pool.factories.BallFactory;
 
 import com.sem.pool.factories.CameraFactory;
 import com.sem.pool.factories.CueFactory;
+import com.sem.pool.factories.GameInitializer;
 import com.sem.pool.factories.SceneFactory;
 import com.sem.pool.factories.TableFactory;
 import com.sem.pool.game.Game;
@@ -36,7 +38,7 @@ public class Pool implements Screen, GameObserver {
     private transient AssetLoader assetLoader;
     private transient ModelBatch modelBatch;
     private transient Scene3D scene;
-    private static final Vector3 cameraPosition = new Vector3(0f, 100f, 0f);
+    private static final Vector3 CAMERA_POSITION = new Vector3(0f, 100f, 0f);
     private transient Game game;
 
     // State flag to keep track of whether asset loading
@@ -63,6 +65,14 @@ public class Pool implements Screen, GameObserver {
         Bullet.init();
     }
 
+    public AssetLoader getAssetLoader() {
+        return assetLoader;
+    }
+
+    public Scene3D getScene() {
+        return scene;
+    }
+
     /**
      * Initializes the asset loader & loads the necessary
      * models into the asset loader.
@@ -81,51 +91,29 @@ public class Pool implements Screen, GameObserver {
      * been completed and the scene is not yet initialized.
      */
     private void initializeScene() {
-        // If the game has not yet been loaded, and an
-        // assetLoader update event is received in current iteration,
-        // then load the game.
-        if (!loaded && assetLoader.getAssetManager().update()) {
-            float width = Gdx.graphics.getWidth();
-            float height = Gdx.graphics.getHeight();
-            CameraFactory cameraFactory = new CameraFactory(width, height, cameraPosition);
+        // Retrieve resolution of the game from inner GDX graphics parameters.
+        Vector2 resolution = new Vector2(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-            Texture cueTexture = assetLoader.getCueTexture();
-            CueFactory cueFactory = new CueFactory(cueTexture, assetLoader);
+        // Create a Game initializer with the Game's internal parameters
+        // required for making a Game.
+        GameInitializer gameInitializer = new GameInitializer(assetLoader, modelBatch,
+                Gdx.input, resolution, CAMERA_POSITION);
 
-            List<Texture> ballTextures = assetLoader.getBallTextures();
-            BallFactory ballFactory = new BallFactory(ballTextures, assetLoader);
+        // Instantiate the game & retrieve the scene from the game
+        game = gameInitializer.createGame();
+        scene = game.getScene();
 
-            Texture tableTexture = assetLoader.getTableTexture();
-            TableFactory tableFactory = new TableFactory(tableTexture, assetLoader);
+        // Update the camera of the scene to point to the right location
+        scene.getCamera().update();
 
-            SceneFactory sceneFactory = new SceneFactory(tableFactory,
-                    ballFactory, cameraFactory, cueFactory, modelBatch);
-
-            // Instantiate the scene
-            scene = sceneFactory.createScene();
-
-            // Update the camera of the scene to point to the right location
-            scene.getCamera().update();
-
-            initializeGame();
-
-            // The assets of the game are now fully loaded
-            loaded = true;
-        }
-    }
-
-    /**
-     * Initializes the Game instance for the current Pool Game.
-     */
-    private void initializeGame() {
-        // Create new game with local scene & GDX input
-        game = Game.createNewGame(scene, Gdx.input);
-
-        // Observe the created Game
+        // Make the Pool game observe the Game loop class to receive events.
         game.addObserver(this);
 
-        // Start the game
+        // Start the Game.
         game.startGame();
+
+        // The assets of the game are now fully loaded
+        loaded = true;
     }
 
     /**
@@ -142,22 +130,18 @@ public class Pool implements Screen, GameObserver {
         }
     }
 
-    public AssetLoader getAssetLoader() {
-        return assetLoader;
-    }
-
-    public Scene3D getScene() {
-        return scene;
-    }
-
     /**
      * Render the scene.
      * @param delta Time in seconds between last render.
      */
     @Override
     public void render(float delta) {
-        // Initialize scene if uninitialized
-        initializeScene();
+        // If the game has not yet been loaded, and an
+        // assetLoader update event is received in current iteration,
+        // then load the game.
+        if (!loaded && assetLoader.getAssetManager().update()) {
+            initializeScene();
+        }
 
         // Clear depth buffer & color buffer masks
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
