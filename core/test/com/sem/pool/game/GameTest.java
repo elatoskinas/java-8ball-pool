5caa905f3d70b1ae28aff477eeb660115a37d96c
Dany Sluijk <D.Sluijk@student.tudelft.nl>
2020-01-16 12:44:52 +0100

    chore: missing renaming of function

--- core/test/com/sem/pool/game/GameTest.java
+++ core/test/com/sem/pool/game/GameTest.java
@@ -259,7 +259,7 @@ public class GameTest extends GameBaseTest {
 
         // Verify ball is potted
         Mockito.verify(ball).pot();
-        Mockito.verify(this.scene).recenterCue(ball);
+        Mockito.verify(this.scene).recenterCueBall(ball);
     }
 
     /**
